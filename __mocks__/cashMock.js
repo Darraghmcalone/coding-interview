ba4c49aa45400233496727c5c66f3ce37780ce85
Darragh McAlone <darraghmcalone@hotmail.co.uk>
2018-08-28 14:33:19 +0100

    add refill inventory test

--- __mocks__/cashMock.js
+++ __mocks__/cashMock.js
@@ -1,39 +1,40 @@
-const cash = {    
-   coin1: {
+module.exports = [
+
+    {
         name: "five pence",
         value: 0.05,
         quantity: 70,
-        
+
     },
-    coin2: {
+    {
         name: "ten pence",
         value: 0.10,
         quantity: 60,
-        
+
     },
-    coin3: {
+    {
         name: "twenty pence",
         value: 0.20,
         quantity: 50,
-        
+
     },
-    coin4: {
+    {
         name: "fifty pence",
         value: 0.50,
         quantity: 40,
-        
+
     },
-    coin5: {
+    {
         name: "one pound",
         value: 1.00,
         quantity: 40,
-        
+
     },
-    coin6: {
+    {
         name: "two pounds",
         value: 2.00,
         quantity: 30,
-        
+
     }
-}
-module.exports = cash;
+]
+
---
+++ __mocks__/inventoryMock.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+
+        product: 'mars',
+        quantity: 2000,
+        price: 1.00
+
+    },
+    {
+
+        product: 'twix',
+        quantity: 6,
+        price: 0.80
+
+    },
+
+    {
+
+        product: 'fredo',
+        quantity: 19,
+        price: 0.20
+
+    }
+
+]
+
